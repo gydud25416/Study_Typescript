d70c0072463cd61937683a78eb4652f6a3531779
gydud25416 <gydud25416@gmail.com>
2024-09-15 23:19:28 +0900

    [Sec2:Chp4] 타입 별칭과 인덱스 시그니처

---
+++ section2/dist/chapter4.js
@@ -0,0 +1,18 @@
+//타입 별칭
+let user = {
+    id: 1,
+    name: "이정환",
+    nickname: "winterlood",
+    birth: "1997.01.07",
+    bio: "안녕하세요",
+    location: "부천시"
+};
+let user2 = {
+    id: 1,
+    name: "이정환",
+    nickname: "winterlood",
+    birth: "1997.01.07",
+    bio: "안녕하세요",
+    location: "부천시"
+};
+export {};
