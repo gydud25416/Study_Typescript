d92fae3804ac507f0afa5e61f7565665d3fbeb8d
gydud25416 <gydud25416@gmail.com>
2024-09-15 21:57:59 +0900

    [Section:Chp3] 객체 타입

---
+++ section2/src/chapter3.ts
@@ -0,0 +1,15 @@
+let user:{
+    id?:number;
+    name:string;
+} = {
+    id:1,
+    name:"이정환",
+}
+
+let config:{
+    readonly apikey:string;
+} ={
+    apikey:'My API KEY'
+}
+
+// config.apikey="hasdf"
